0177a3e79c3f3e22cce2de257387a721dd23def4
Rita Morais <fc38844@alunos.fc.ul.pt>
2021-10-14 09:32:36 +0100

    Versão 2

--- app/src/main/java/com/example/paint/MainActivity.java
+++ app/src/main/java/com/example/paint/MainActivity.java
@@ -1,19 +1,20 @@
 package com.example.paint;
 
+import android.app.Fragment;
 import android.content.Intent;
 import android.content.res.Resources;
-import android.graphics.Color;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private static int BACK_COLOR;
-    private final int REQUEST_CODE = 1;
 
     @Override
     public Resources getResources() {
@@ -25,58 +26,65 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.layoutMain).setBackgroundColor(BACK_COLOR);
     }
 
-    protected  void onActivityResult(int requestCode, int resultCode, Intent data){
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == REQUEST_CODE && data != null){
-            Bundle extras = data.getExtras();
-            String color = extras.getString("color");
-
-                switch(color) {
-                    case "1":
-                        BACK_COLOR = getResources().getColor(R.color.c1);
-                        break;
-                    case "2":
-                        BACK_COLOR = getResources().getColor(R.color.c2);
-                        break;
-                    case "3":
-                        BACK_COLOR = getResources().getColor(R.color.c3);
-                        break;
-                    case "4":
-                        BACK_COLOR = getResources().getColor(R.color.c4);
-                        break;
-                    case "5":
-                        BACK_COLOR = getResources().getColor(R.color.c5);
-                        break;
-                    case "6":
-                        BACK_COLOR = getResources().getColor(R.color.c6);
-                        break;
-                    case "7":
-                        BACK_COLOR = getResources().getColor(R.color.c7);
-                        break;
-                    case "8":
-                        BACK_COLOR = getResources().getColor(R.color.c8);
-                        break;
-                    default:
-                        BACK_COLOR = getResources().getColor(R.color.c9);
-                }
-                findViewById(R.id.layoutMain).setBackgroundColor(BACK_COLOR);
+    /* Called when the user taps a color button */
+    public void changeBackColor(View view) {
+        switch(view.getId()) {
+            case R.id.button1:
+                BACK_COLOR = getResources().getColor(R.color.c1);
+                break;
+            case R.id.button2:
+                BACK_COLOR = getResources().getColor(R.color.c2);
+                break;
+            case R.id.button3:
+                BACK_COLOR = getResources().getColor(R.color.c3);
+                break;
+            case R.id.button4:
+                BACK_COLOR = getResources().getColor(R.color.c4);
+                break;
+            case R.id.button5:
+                BACK_COLOR = getResources().getColor(R.color.c5);
+                break;
+            case R.id.button6:
+                BACK_COLOR = getResources().getColor(R.color.c6);
+                break;
+            case R.id.button7:
+                BACK_COLOR = getResources().getColor(R.color.c7);
+                break;
+            case R.id.button8:
+                BACK_COLOR = getResources().getColor(R.color.c8);
+                break;
+            case R.id.button9:
+                BACK_COLOR = getResources().getColor(R.color.c9);
+                break;
+            case R.id.button10:
+                BACK_COLOR = getResources().getColor(R.color.c10);
+                break;
+            case R.id.button11:
+                BACK_COLOR = getResources().getColor(R.color.c11);
+                break;
+            default:
+                BACK_COLOR = getResources().getColor(R.color.c12);
         }
+        findViewById(R.id.fragment_canvas).setBackgroundColor(BACK_COLOR);
     }
 
-    /* Called when the user taps the settings button */
-    public void showSettings (View view){
-        Intent intent = new Intent(this, ShowSettings.class);
-        startActivityForResult(intent, REQUEST_CODE);
+    public void showPalette(View view){
+        View v = findViewById(R.id.fragment_palette);
+        if(v.getVisibility() == View.VISIBLE)
+            v.setVisibility(View.INVISIBLE);
+        else
+            v.setVisibility(View.VISIBLE);
     }
 
     /* Called when the user taps the about button */
     public void about (View view){
         Intent intent = new Intent(this, About.class);
         startActivity(intent);
-
     }
+
+
+
 
 }
